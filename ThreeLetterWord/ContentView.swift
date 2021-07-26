2920e8841e419a81636203d43855acb82e9196e4
Tucker Gillbrand <tucker@gillbrand.com>
2021-07-26 10:48:42 -0700

    initial commit

---
+++ ThreeLetterWord/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThreeLetterWord
+//
+//  Created by Tucker(School) on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThreeLetterWord/ThreeLetterWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeLetterWordApp.swift
+//  ThreeLetterWord
+//
+//  Created by Tucker(School) on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ThreeLetterWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
